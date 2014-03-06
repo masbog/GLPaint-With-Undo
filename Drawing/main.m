b2e63b5de52da99ea41fa28488eb24d534539717
Augusta Bogie <masbog@me.com>
2014-03-06 22:52:59 +0700

    Initial Commit

---
+++ Drawing/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Drawing
+//
+//  Created by Augusta Bogie on 3/6/14.
+//  Copyright (c) 2014 Augusta Bogie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MASAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MASAppDelegate class]));
+    }
+}
